0150caf5925cddd06566c034e0ed5b9e0f704cb8
sadiakabir811 <sadiakabir811@gmail.com>
2026-04-20 02:35:49 +0600

    ml-service backend

--- ml-service/app.py
+++ ml-service/app.py
@@ -34,4 +34,5 @@ def predict():
         }), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=True)
