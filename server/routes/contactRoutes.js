2a74c2d4e1d63a53b24128736d9a6490da83c9bf
sadiakabir811 <sadiakabir811@gmail.com>
2026-04-21 20:32:04 +0600

    error fixing

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { adminOnly } from "../middleware/adminMiddleware.js";
 import {
     submitContactMessage,
     getAllContactMessages,
